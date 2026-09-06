c7ab9c9f7c6ffb7133c27010ce78a548bb4f41d5
pls455 <karamhahhss123@gmail.com>
2026-09-06 19:40:39 +0300

    Add Firebase project configuration template

---
+++ js/config.js
@@ -0,0 +1,3 @@
+// Paste the Firebase Web App configuration from Firebase Console here.
+// These values are identifiers, not private server secrets.
+export const firebaseConfig={apiKey:'',authDomain:'',projectId:'',storageBucket:'',messagingSenderId:'',appId:''};
